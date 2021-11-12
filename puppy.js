81c2ac21a1dd2f21fda703f7951634b01a094632
Madi Combe <54414117+MadiCombe@users.noreply.github.com>
2021-11-12 01:06:32 -0600

    updates

--- puppy.js
+++ puppy.js
@@ -7,3 +7,34 @@ const puppySchema = mongoose.Schema({
  
 module.exports = mongoose.model("Puppy", 
 puppySchema) 
+// for a specific Costume. 
+exports.puppy_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Puppy.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+// Handle Costume update form on PUT. 
+exports.puppy_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await puppy.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.puppy_breed)  
+               toUpdate.puppy_breed = req.body.puppy_breed; 
+        if(req.body.weight) toUpdate.weight = req.body.weight; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
